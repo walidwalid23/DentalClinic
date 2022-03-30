884cf93ffcea1213084496f1c381bbc40a44d8cb
Walid <walid.tawfik200@gmail.com>
2022-03-30 18:20:27 +0200

    commit

---
+++ database/db.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class DB{
+
+private $host_name="localhost";
+private $username="root";
+private $password="";
+private $db_name="dentalclinic";
+
+protected function connect(){
+
+$db_obj=mysqli_connect($this->host_name,$this->username,$this->password,$this->db_name);
+if(!$db_conn){ die("Couldn't Connect To The Database");}
+else{
+
+    return $db_obj;
+}
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+?>
