fa56c21686a83b2133b07e7ecb7fabdc3f5ee0e9
Seif-Nafaa <102489185+Seif-Nafaa@users.noreply.github.com>
2022-06-03 22:54:29 +0200

    Update deleteappointment.route.php

--- routes/deleteappointment.route.php
+++ routes/deleteappointment.route.php
@@ -1,3 +1,15 @@
 <?php
+include '../controllers/appointmentcontroller.class.php';
 
+$appointmentID = $_GET["appointmentid"];
+
+$appointmentCont = new AppointmentController("","");
+
+if($appointmentCont->removeAppointment($appointmentID)){
+    header("location:../views/pages/dashboard.php");
+}
+
+else{
+    die("Couldn't Delete The Appointment");
+}
 ?>
