d06d0d7f9f70c8221a4dca48699dd594ca688893
Walid <walid.tawfik200@gmail.com>
2022-06-03 22:35:26 +0200

    various updates

---
+++ views/pages/deletesecretary.php
@@ -0,0 +1,121 @@
+<!--CHECK FOR AUTHORIZATION --> 
+<?php
+ 
+ include '../../generalclasses/authorization.class.php';
+
+ if(!Authorization::isAllowed()){
+  header("Location:unauthorized.php");
+ }
+
+
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Delete Secretary</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- Favicon -->
+    <link href="../../img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600;700&family=Open+Sans:wght@400;600&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="../../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../../lib/animate/animate.min.css" rel="stylesheet">
+    <link href="../../lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="../../lib/twentytwenty/twentytwenty.css" rel="stylesheet" />
+    <!--Axios Library-->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="../../css/style.css" rel="stylesheet">
+    <!-- Page Stylesheet -->
+    <link href="../../css/deletesecretary.css" rel="stylesheet">
+</head>
+
+<body>
+      <!-- Spinner Start -->
+      <?php
+   include '../../includes/spinner.inc.php';
+    ?>
+    <!-- Spinner End -->
+    <!-- Topbar Start -->
+    <?php
+   include '../../includes/topbar.inc.php';
+    ?>
+    <!-- Topbar End -->
+
+    <!-- Navbar Start -->
+    <?php
+   include '../../includes/navbar.inc.php';
+   display_navbar("");
+    ?>
+    <!-- Navbar End -->
+
+    <!-- Hero Start -->
+    <div class="container-fluid bg-primary py-5 hero-header mb-5">
+        <div class="row py-3">
+            <div class="col-12 text-center">
+                <h1 class="display-3 text-white animated zoomIn">Delete Secretary</h1>
+            </div>
+        </div>
+    </div>
+<!-- Hero End -->
+<!-- Add Secretary Form Start -->
+            <div class="col-xl-4 col-lg-6">
+                <form id="delete-secretary-form" >
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <input type="email" id="email-field" class="form-control border-0 bg-light px-4" name="email"  maxlength="70" placeholder="Secretary Email"
+                            required>
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-danger w-100 py-3" id="delete-button" type="submit">Delete Secretary</button>
+                            <p id="response-message"></p>
+
+                        </div>
+
+                         
+                    </div>
+                </form>
+            </div>
+
+<!-- Add Secretary Form End -->
+
+
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../lib/wow/wow.min.js"></script>
+    <script src="../../lib/easing/easing.min.js"></script>
+    <script src="../../lib/waypoints/waypoints.min.js"></script>
+    <script src="../../lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="../../lib/tempusdominus/js/moment.min.js"></script>
+    <script src="../../lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="../../lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="../../lib/twentytwenty/jquery.event.move.js"></script>
+    <script src="../../lib/twentytwenty/jquery.twentytwenty.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="../../js/main.js"></script>
+    <!-- Delete Secretary Javascript -->
+    <script src="../../js/deletesecretary.js"></script>
+</body>
+
+</html>
